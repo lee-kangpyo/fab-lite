c702ac601423285492b08f8014fe412716bb4814
그램 <darkzhon@hanmail.net>
2026-05-04 21:15:31 +0900

    feat: 프론트엔드 연결을 위한 CORS 미들웨어 추가

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.tasks import router as tasks_router
 from app.api.chat import router as chat_router
@@ -47,6 +48,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="FabriX Lite", version="0.4.0", lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(tasks_router)
 app.include_router(chat_router)
 
